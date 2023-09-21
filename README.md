aa8414530cad5268297a86d813b886d6e4575ea8
zahid <zahid@zulunity.com>
2023-09-21 02:24:41 -0600

    Add Grant Reader Script

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # aws-grant-reader
 AWS Grant Reader Script
+
+## Description
+
+This script will create
+
+An s3 bucket with versioning to store tf state created.
+
+Create a Role with:
+    ReadOnly Access
+    Trust Relationship to the TRUSTED_ACCOUNT_ID
+
+## Execution
+
+- Log into your AWS CloudShell and execute (Replacing TRUSTED_ACCOUNT_ID with the account id)
+
+```sh
+bash -c "$(curl https://raw.githubusercontent.com/zulunity/aws-grant-reader/main/script.sh)" -s TRUSTED_ACCOUNT_ID
+```
+
---
+++ script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+export TRUSTED_ACCOUNT_ID=${1}
+echo "========== Script to create a bucket that will preserve the state of a Reader Role for the TRUSTED_ACCOUNT_ID: $TRUSTED_ACCOUNT_ID ========"
+export AWS_ACCOUNT_ID="$(aws sts get-caller-identity  | jq -r .Account)"
+# Create a Bucket Name with the account and region
+BUCKET="zulunity-remote-state-$AWS_ACCOUNT_ID-$AWS_REGION"
+# Create Bucket for remote storage
+aws s3api create-bucket \
+    --bucket $BUCKET \
+    --region $AWS_REGION
+aws s3api put-bucket-versioning --bucket $BUCKET --versioning-configuration Status=Enabled
+# Install yum-utils
+sudo yum install -y yum-utils
+# Use yum-config-manager to add the official HashiCorp Linux repository.
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# Install terraform 
+sudo yum -y install terraform
+# Clone aws-reader-role
+git clone https://github.com/zulunity/aws-reader-role.git
+# Enter into the repo 
+cd aws-reader-role
+# Terraform 
+echo -en 'terraform {\n  backend "s3" {}\n}' > backend.tf
+export TF_VAR_account_id="$TRUSTED_ACCOUNT_ID"
+export TF_VAR_description="Role grating acces from $AWS_ACCOUNT_ID to $TRUSTED_ACCOUNT_ID as reader"
+terraform init \
+    -backend-config="bucket=$BUCKET" \
+    -backend-config="key=state" \
+    -backend-config="region=$AWS_REGION"
+terraform apply -auto-approve
